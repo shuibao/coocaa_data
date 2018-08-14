df91d49490f993300638536fba4b54ca1e5c8f63
liangxuqing <421918666@qq.com>
2018-08-14 18:12:37 +0800

    1.0数据导入

---
+++ src/main/java/com/example/datamanage/DatamanageApplication.java
@@ -0,0 +1,12 @@
+package com.example.datamanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class DatamanageApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DatamanageApplication.class, args);
+    }
+}
